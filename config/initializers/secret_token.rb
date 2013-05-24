b124246b95c273eca48549fbf5669c62988c9977
Keiran <keiran@carpela.com>
2013-05-24 16:35:27 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Carpela::Application.config.secret_token = '36a2caedcb799cc4372b0e651b2d7478c625a7cd45ed06c085908b18ea0281626d804f4433d75160cc815eea99652d14063b4026ab36285e5aa31fc4cb65871e'
